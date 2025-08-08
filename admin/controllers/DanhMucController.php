2657e290f2de589c13e3333561ccf5023d7db500
Ho Minh <minhhdph48234@fpt.edu.vn>
2025-08-08 14:56:23 +0700

    xong edit danh mục

--- admin/controllers/DanhMucController.php
+++ admin/controllers/DanhMucController.php
@@ -41,7 +41,20 @@ class DanhMucController
         }
     }
 
-    
+    public function formEditDanhMuc()
+    {
+        $id = $_GET['id']; // Use null coalescing to handle missing ID
+
+        if ($id) {
+            $danhMuc = $this->modelDanhmuc->getDetailDanhMuc($id);
+            if ($danhMuc) {
+                require_once './views/danhmuc/suadm.php';
+                return;
+            }
+        }
+        header("Location: " . BASE_URL_ADMIN . '?act=listdm');
+        exit();
+    }
 
 
     public function postEditDanhMuc()
